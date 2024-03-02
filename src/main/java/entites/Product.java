ec757a4a9296364264af6a5d47cec3fd9e5f3710
Latha <latha.r@mpokket.com>
2024-03-02 22:18:57 +0530

    added rest profile logging helth checksecurity

--- src/main/java/entites/Product.java
+++ src/main/java/entites/Product.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 @Entity
 public class Product {
    @Id
-   @GeneratedValue(strategy = GenerationType.AUTO)
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String description;
